7588567798c9a64b8776f192a491909fc6caccd7
yunju6007-art <yunju6007@gmail.com>
2026-04-23 22:25:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const PIXEL_ID = '2079561172587594';
-const ACCESS_TOKEN = '你的AccessToken';
+const ACCESS_TOKEN = 'EAAg9kpg2aZCsBRQyTkVTqUB0IWdBSsXArFmhKIV8D8hZAD7rkkTjhP8Af5Ahbc1p4y2a9f5d698reyse7702hp4k0p7mzcfqa92c9ef5rpq6g7u293yqs3vvdkI5iWWq5ZACEMkAArUGSAZAqTKLZAnvSvX4ZC3Md08dMqe2kW7arOJaMtLb0qucshZAEKMZACZAq9Wj4O9Pbc1p4y2a9f5d698reyse7702hp4k0p7mzcfqa92c9ef5rpq6g7u293yqs3vvdk';
 
 app.post('/track', async (req, res) => {
   try {
